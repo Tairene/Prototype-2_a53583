9b54056150f4787ba3183f7fcd70bc050e1e9b67
ª <162058354+Tairene@users.noreply.github.com>
2024-05-28 20:04:16 +0100

    Jogar Fetch

---
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    public float spawnCooldown = 1.0f; // Tempo de espera entre as gerações de cão
+    private float lastSpawnTime; // Tempo da última geração de cão
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time - lastSpawnTime >= spawnCooldown)
+        {
+            SpawDog();
+        }
+    }
+    void SpawDog()
+    {
+        Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+        lastSpawnTime = Time.time; // Atualiza o tempo da última geração de cão
+    }
+}
